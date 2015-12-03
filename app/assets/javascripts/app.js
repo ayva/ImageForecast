df9a61453535cf4382be128d27d312fa95d79a6c
ayva <olga.ayva@gmail.com>
2015-12-03 15:29:45 -0800

    about page, search button hide, show

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -38,10 +38,26 @@ var futugram = angular.module('futugram',['restangular', 'ui.router','ui.bootstr
       })
       .state('about',{
         url: '/about',
+        resolve: {
+          current_user: function($http, storage){
+            return $http.get('authorized.json').then(function(response){
+              console.log("Authorized?",  response.data);
+              
+              storage.current_user.data = response.data;
+              return response.data;
+            }, function(error){
+              console.log("Resolve current user failed", error) ;
+              return false;
+            });
+          }
+        },
         views: {
           '':{
-            templateUrl: 'templates/about.html',
-            controller: 'aboutCtrl'
+            templateUrl: 'templates/about.html'
+          },
+          'navbar' : {
+            templateUrl: 'partials/navbar.html',
+            controller: 'navbarCtrl'
           }
         }
       });
